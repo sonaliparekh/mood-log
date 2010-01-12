5b2f2b56343ae4bc0a75349f4eff0dea9cf84496
burke.eric <burke.eric@c419ba3c-fec4-11de-b8ab-b3eb4b1fa784>
2010-01-12 02:02:02 +0000

    added a crude ListAdapter to ViewLogActivity

--- src/com/stuffthathappens/moodlog/ViewLogActivity.java
+++ src/com/stuffthathappens/moodlog/ViewLogActivity.java
@@ -1,7 +1,13 @@
 package com.stuffthathappens.moodlog;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ViewLogActivity extends ListActivity {
     
@@ -9,6 +15,69 @@ public class ViewLogActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_log);
+        
+        MoodLog ml = ((MoodLogApp) getApplication()).getMoodLog();
+        setListAdapter(new LogListAdapter(this, ml));
     }
     
+    private class LogListAdapter extends BaseAdapter {
+    	
+    	private final MoodLog mMoodLog;
+    	private final Context mContext;
+    	
+    	LogListAdapter(Context c, MoodLog ml) {
+    		mContext = c;
+    		mMoodLog = ml;
+    	}
+
+		@Override
+		public int getCount() {
+			return mMoodLog.getLogEntryCount();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return mMoodLog.getLogEntry(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return mMoodLog.getLogEntry(position).getDate().getTime();
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LogEntryView v;
+			if (convertView == null) {
+				v = new LogEntryView(mContext, mMoodLog.getLogEntry(position));
+			} else {
+				v = (LogEntryView) convertView;
+				v.setLogEntry(mMoodLog.getLogEntry(position));
+			}
+			return v;
+		}
+    }
+    
+    private class LogEntryView extends LinearLayout {
+    	
+    	private final TextView mWordView;
+    	// TODO fields for date
+    	
+    	public LogEntryView(Context context, LogEntry logEntry) {
+    		super(context);
+    		
+    		setOrientation(VERTICAL);
+    		
+    		mWordView = new TextView(context);
+    		mWordView.setText(logEntry.getWord());
+    		// TODO font size
+    		
+    		addView(mWordView, new LinearLayout.LayoutParams(
+    				LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+    	}
+    	
+    	public void setLogEntry(LogEntry logEntry) {
+    		mWordView.setText(logEntry.getWord());
+    	}
+    }
 }
